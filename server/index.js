80b1e5511f59a56a9b0a1f03ad279b9ae8a7e39e
becooliu <243257686@qq.com>
2024-06-05 23:42:12 +0800

    1.format file

--- server/index.js
+++ server/index.js
@@ -1,29 +1,33 @@
-const express = require('express')
-const app = express()
+"use strict";
+const express = require("express");
+const app = express();
 
-const api = require('./api/user/addUser')
+const api = require("./api/user/userApi");
 
-
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 //解析 application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 //解析 application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-const path = require('path')
+const path = require("path");
 // 访问静态资源文件 这里是访问所有dist目录下的静态资源文件
-app.use(express.static(path.resolve(__dirname, '../public/Uploads/images')));
-
+app.use(express.static(path.resolve(__dirname, "../public/Uploads/images")));
 
 //允许跨域访问 方法1：
-const cors = require('cors')
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['PUT,POST,GET,DELETE,OPTIONS'],
-  credentials: true, // 是否允许带上cookie 信息
-  allowedHeaders: 'Content-Type'
-}))
-
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["PUT,POST,GET,DELETE,OPTIONS"],
+    credentials: true, // 是否允许带上cookie 信息
+    allowedHeaders: "Content-Type",
+  })
+);
 
 //允许跨域访问 方法2：
 
@@ -38,9 +42,9 @@ app.use(cors({
     }
   }); */
 
-  app.use(api)
+app.use(api);
 
-  const port = 8088
-  app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-  })
+const port = 8088;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
--- server/model/db.js
+++ server/model/db.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+
+// const userSchema2 = require("./schema");
 
 /* const DBURL = 'mongodb:127.0.0.1:27017/common'
 mongoose.connect(DBURL).then(res => {
@@ -8,36 +10,36 @@ mongoose.connect(DBURL).then(res => {
 }) */
 
 //连接数据库
-mongoose.connect('mongodb://127.0.0.1:27017/common');
+mongoose.connect("mongodb://127.0.0.1:27017/common");
 
 //为连接绑定事件
 const db = mongoose.connection;
-db.once('error', ()=>console.log('database connect error.'));
-db.once('open' , ()=> console.log('Mongo connect success .'));
+db.once("error", () => console.log("database connect error."));
+db.once("open", () => console.log("Mongo connect success ."));
 
 const userSchema = mongoose.Schema(
-    {
-        username: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        age: Number,
-        sex: {
-            type: String,
-            default: '未知'
-        }
+  {
+    username: {
+      type: String,
+      required: true,
     },
-    {
-        versionKey: false
-    }
-)
+    password: {
+      type: String,
+      required: true,
+    },
+    age: Number,
+    sex: {
+      type: String,
+      default: "未知",
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
 
 const Models = {
-    userModel: mongoose.model('user', userSchema, 'user')
-}
+  userModel: mongoose.model("user", userSchema, "user"),
+};
 
-module.exports = Models
+module.exports = Models;
