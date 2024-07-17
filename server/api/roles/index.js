fa19c6048ab96bd91ce40734eca8778b1adb98d6
becooliu <243257686@qq.com>
2024-07-17 19:33:24 +0800

    新增创建角色接口

--- server/api/roles/index.js
+++ server/api/roles/index.js
@@ -3,28 +3,56 @@ const Roles = require("../../model/Roles");
 const express = require("express");
 const router = express.Router();
 
+let resData = {}
+router.use((req, res, next) => {
+  console.log('req.userInfo', Boolean(req?.userInfo))
+  if(req?.userInfo && !req.userInfo?.isAdmin) {
+      res.json({message: '对不起，只有管理员才可以进入该页面。', status: 203})
+      return
+    }
+    resData.status = ''
+    resData.message = ''
+  next()
+})
+
 router.post('/roles/create', async(req, res) => {
-    let resData = {}
     try {
-        const {role, permission} = req.body
-        let roleData = Roles.findOne({role})
+        const {role, permissions} = req.body
+        let roleData = await Roles.findOne({role})
+        // 如果不存在此角色，则直接新增
         if(!roleData) {
             const newRole = new Roles({
                 role,
-                permission
+                permissions: [permissions]
             })
 
             newRole.save().then(result => {
                 resData.status = 200
                 resData.message = '新增角色成功'
                 resData.role = role
+                res.json(resData)
             })
 
-            res.json(resData)
         }else {
-            resData.status = 210
-            resData.message = `角色 ${role} 已存在，新增失败`
-            res.json(resData)
+            //如果角色和权限是否同时存在
+            const roleAndPermissionExist = (role == roleData.role && roleData.permissions.includes(permissions))
+            if(!roleAndPermissionExist) {
+                const updateRoleAndPermission = await Roles.findOneAndUpdate({role}, {$push: {permissions}}, {new: true})
+                if(!updateRoleAndPermission) {
+                    // 数据更新失败
+                    resData.status = 300
+                    resData.message = '角色更新失败，请稍后再试'
+                }else {
+                    resData.status = 200
+                    resData.message = '角色更新成功'
+                }
+                res.json(resData)
+            } else {
+                //  如果角色和权限都存在
+                resData.status = 210
+                resData.message = `角色 ${role} 已存在，新增失败`
+                res.json(resData)
+            }
         }
     } catch (error) {
         console.log(error)
