d2c23462b2de4e86d3c7bf10bcd9527b91d5f3db
becooliu <243257686@qq.com>
2024-07-23 14:59:54 +0800

    增加api 访问量统计接口

---
+++ server/api/statistics/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express()
+
+const apiVisited = require('../../model/statistics/apiVisit')
+
+let resData = {}
+router.use((req, res, next) => {
+  resData.status = ''
+  resData.message = ''
+  next()
+})
+
+router.get('/getApiVisited', async(req, res) => {
+    try {
+        let data = await apiVisited.countDocuments()
+        resData.status = 200
+        resData.count = data
+        res.json(resData)
+
+    } catch (error) {
+        resData.message = error
+        res.json(resData)
+    }
+})
+
+module.exports = router
