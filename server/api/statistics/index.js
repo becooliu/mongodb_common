b3489d06bfc6ae62e482c095356d875db57045c6
becooliu <243257686@qq.com>
2025-03-17 18:24:40 +0800

    新增24小时接口访问量统计接口

--- server/api/statistics/index.js
+++ server/api/statistics/index.js
@@ -3,21 +3,33 @@ const router = express()
 
 const apiVisited = require('../../model/statistics/apiVisit')
 
-let resData = {}
-router.use((req, res, next) => {
-  resData.status = ''
-  resData.message = ''
-  next()
-})
-
 /**
  * api 访问次数统计
  */
 router.get('/getApiVisited', async (req, res) => {
+  let resData = {}
   try {
     let data = await apiVisited.countDocuments()
     resData.status = 200
     resData.count = data
+    res.json(resData)
+  } catch (error) {
+    resData.message = error
+    res.json(resData)
+  }
+})
+
+/**
+ * 24小时内 api 访问次数统计
+ */
+router.get('/get24HoursApiVisited', async(req, res) => {
+  const resData = {}
+  const now = new Date()
+  let before24Hours = new Date(now.getTime() - 24 * 60 * 60 * 1000)
+  try {
+    let data = await apiVisited.find({visitTime : {$gte: before24Hours, $lte: now}})
+    resData.status = 200
+    resData.count = data.length
     res.json(resData)
   } catch (error) {
     resData.message = error
